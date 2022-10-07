d135c820236da97a25bceb5fe47767d6a7b60617
jwk000 <j__w__k@163.com>
2022-10-07 20:39:23 +0800

    101not end

--- Algorithm/src/Poker101.cs
+++ Algorithm/src/Poker101.cs
@@ -29,10 +29,64 @@ namespace Algorithm
      */
     public class Poker101
     {
-        
+
         public List<List<int>> BestHandCard(List<int> cards)
         {
+            //思路：穷举所有情况，判断最大牌型
+            //先排序，方便查找相邻数字牌
+            //取cards[i]判断是否组成刻子K，是则取rk = K+max(k-Rest)
+            //取cards[i]判断是否组成顺子S，是则取rs = S+max(s-Rest)
+            //max(cards) = max(rk,rs)
+
+            cards.Sort();
+
+            int s = cards[0];
+
+
             return null;
+        }
+
+        Dictionary<HandCardStatus, Result> mCache = new Dictionary<HandCardStatus, Result>();
+
+
+        class Result
+        {
+            public int value;
+            public List<List<int>> combo = new List<List<int>>();
+            public HandCardStatus status;
+        }
+
+
+        class HandCardStatus
+        {
+            long Low;
+            long High;
+
+
+            public HandCardStatus(List<int> cards)
+            {
+                foreach (int card in cards)
+                {
+                    int idx = (card / 100 - 1) * 13 + (card % 100 - 1);
+                    if ((Low & (1 << idx)) == 0)
+                    {
+                        Low |= 1 << idx;
+                    }
+                    else
+                    {
+                        High |= 1 << idx;
+                    }
+                }
+            }
+
+            public override int GetHashCode()
+            {
+                int v = (int)Low;
+                v ^= (int)(Low >> 32) * 13;
+                v ^= (int)(High) * 13;
+                v ^= (int)(High >> 32) * 13;
+                return v;
+            }
         }
     }
 }
