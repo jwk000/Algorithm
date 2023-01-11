8446ba870d3b974c5031432fc77a0cf3d4fd4ec6
jwk's github <j__w__k@163.com>
2023-01-11 19:10:38 +0800

    前缀和数组，前缀和矩阵

---
+++ Algorithm/src/PreSumArray.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    //前缀和数组：用于快速求解一维数组[i,j]区间内元素和
+    public class PreSumArray
+    {
+        int[] mPreSum;
+        public PreSumArray(int[] arr)
+        {
+            mPreSum = new int[arr.Length];
+            mPreSum[0] = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                mPreSum[i] = mPreSum[i - 1] + arr[i];
+            }
+        }
+
+        public int Sum(int i, int j)
+        {
+            if (i < 0 || j < 0 || i > mPreSum.Length - 1 || j > mPreSum.Length - 1) return -1;
+            if (i == 0) return mPreSum[j];
+            return mPreSum[j] - mPreSum[i - 1];
+        }
+    }
+
+    //前缀和矩阵：用于快速求解二维数组(x1,y1)到(x2,y2)矩形区间内的元素和
+    public class PreSumMatrix
+    {
+        int[,] mPreSum;
+        int mColNum, mRowNum;
+        public PreSumMatrix(int[,] matrix)
+        {
+            mRowNum = matrix.GetLength(0);
+            mColNum = matrix.GetLength(1);
+            mPreSum = new int[mRowNum, mColNum];
+            for (int x = 0; x < mRowNum; x++)
+            {
+                for (int y = 0; y < mColNum; y++)
+                {
+                    int LeftSum = 0;
+                    int UpSum = 0;
+                    int LeftUpSum = 0;
+                    if (x > 0) LeftSum = mPreSum[x - 1, y];
+                    if (y > 0) UpSum = mPreSum[x, y - 1];
+                    if (x > 0 && y > 0) LeftUpSum = mPreSum[x - 1, y - 1];
+                    mPreSum[x, y] = LeftSum + UpSum - LeftUpSum + matrix[x, y];
+                }
+            }
+        }
+
+        public int Sum(int x1, int y1, int x2, int y2)
+        {
+            if (x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0) return -1;
+            if (x1 + 1 > mRowNum || x2 + 1 > mRowNum || y1 + 1 > mColNum || y2 + 1 > mColNum) return -1;
+            int minX = x1 < x2 ? x1 : x2;
+            int maxX = x1 + x2 - minX;
+            int minY = y1 < y2 ? y1 : y2;
+            int maxY = y1 + y2 - minY;
+
+            int Left = 0;
+            int Up = 0;
+            int LeftUp = 0;
+            if (minX > 0) Left = mPreSum[minX - 1, maxY];
+            if (minY > 0) Up = mPreSum[maxX, minY - 1];
+            if (minX > 0 && minY > 0) LeftUp = mPreSum[minX - 1, minY - 1];
+            int ret = mPreSum[maxX, maxY] - Left - Up + LeftUp;
+            return ret;
+        }
+    }
+}
---
+++ TestAlgorithm/TestPreSumArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm;
+
+namespace TestAlgorithm
+{
+    [TestClass]
+    public class TestPreSumArray
+    {
+        [TestMethod]
+        public void TestPreSumArray1()
+        {
+            PreSumArray arr = new PreSumArray(new int[] { 1,2,3,4,5,6,7,8,9,10});
+            int sum = arr.Sum(3, 8);
+            Assert.IsTrue(sum == 39);
+        }
+
+        [TestMethod]
+        public void TestPresumMatrix()
+        {
+            int[,] mat = new int[6, 9];
+            for(int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    mat[i, j] = i * 10 + j;
+                }
+            }
+            PreSumMatrix m = new PreSumMatrix(mat);
+            int sum = m.Sum(2, 3, 5, 7);
+            Assert.IsTrue(sum == 800);
+        }
+    }
+}
