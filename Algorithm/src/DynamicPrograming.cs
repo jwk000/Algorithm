b82ea02d88900537d721bc8b29ddc101d5030118
jwk's github <j__w__k@163.com>
2023-01-16 20:59:52 +0800

    动态规划

--- Algorithm/src/DynamicPrograming.cs
+++ Algorithm/src/DynamicPrograming.cs
@@ -7,13 +7,15 @@ using System.Threading.Tasks;
 namespace Algorithm
 {
     /*
-     * 十大动态规划问题
-     * 动态规划（Dynamic Programming）是求多阶段决策过程（Multistep Decision Process）最优化的一种数学方法。
+     * 动态规划（Dynamic Programming）
+     * 动态规划是求多阶段决策过程（Multistep Decision Process）最优化的一种数学方法。
      * 它将问题的整体按时间或空间的特征分成若干个前后衔接的时空阶段，把多阶段决策问题表示为前后有关的一系列单阶段决策问题，然后逐个求解，从而求出整个问题的最有决策序列。
      * 
-     *  -- 最大子序列和
+     * 经典问题：
      *  -- 零钱兑换
+     *  -- 最大子序列和
      *  -- 最长上升子序列
+     *  -- 最长公共子序列(LCS)
      *  -- 堆盒子
      *  -- 01背包问题
      *  -- 完全背包
@@ -27,6 +29,42 @@ namespace Algorithm
      */
     public class DynamicPrograming
     {
+
+        /*
+         * 零钱兑换
+         * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+         * 输入: coins = [1, 2, 5], amount = 11 输出: 3 解释: 11 = 5 + 5 + 1
+         */
+        public static int MakeChange(int[] coins, int amount)
+        {
+            //dp[i]表示凑成面额为i的最少硬币数量
+            int[] dp = new int[amount + 1];
+
+            foreach (int c in coins)
+            {
+                if (c <= amount)
+                {
+                    dp[c] = 1;
+                }
+            }
+            for (int i = 1; i < amount; i++)
+            {
+                if (dp[i] > 0)
+                {
+                    foreach (int c in coins)
+                    {
+                        int d = i + c;//新的面值
+                        if (d <= amount && (dp[d] == 0 || dp[d] > dp[i] + 1))
+                        {
+                            dp[d] = dp[i] + 1;
+                        }
+                    }
+                }
+            }
+
+            return dp[amount] > 0 ? dp[amount] : -1;
+        }
+
         /*
          * 最大子序列和
          * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和
@@ -34,29 +72,89 @@ namespace Algorithm
          */
         public static int MaxContiguousSubsequence(int[] nums)
         {
-
+            //dp[i]=所有以i结尾的序列的最大和
+            int[] dp = new int[nums.Length];
+            dp[0] = nums[0];
+            int ans = dp[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                dp[i] = Math.Max(nums[i], dp[i - 1] + nums[i]);
+                if (dp[i] > ans) ans = dp[i];
+            }
+            return ans;
         }
 
-        /*
-         * 零钱兑换
-         * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
-         * 输入: coins = [1, 2, 5], amount = 11 输出: 3 解释: 11 = 5 + 5 + 1
-         */
-
-        public static int MakeChange(int[] coins, int amount)
-        {
-
-        }
 
         /*
-         * 最长上升子序列（简称LIS问题）
+         * 最长上升子序列（LIS）
          * 给定一个无序的整数数组，找到其中最长上升子序列的长度。
          * 输入: [10,9,2,5,3,7,101,18] 输出: 4 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
          */
 
         public static int LongestIncreasingSubsequence(int[] arr)
         {
+            if (arr.Length < 2) return arr.Length;
 
+            //dp[i]表示长度为i的最长上升子序列的最后一个元素
+            List<int> dp = new List<int>();
+            dp.Add(arr[0]);
+
+            for (int j = 1; j < arr.Length; j++)
+            {
+                if (arr[j] > dp.Last())
+                {
+                    dp.Add(arr[j]);
+                }
+                for (int i = 0; i < dp.Count; i++)
+                {
+                    //dp数组就是LIS，所以只替换第一个比arr[j]大的dp[i]
+                    if (arr[j] <= dp[i])
+                    {
+                        dp[i] = arr[j];
+                        break;
+                    }
+                }
+
+            }
+            return dp.Count;
+        }
+
+        /*
+         * 最长公共子序列(LCS)
+         * A和B的公共子序列中长度最长的（包含元素最多的）用序列1,3,5,4,2,6,8,7和序列1,4,8,6,7,5为例，它们的最长公共子序列有1,4,8,7和1,4,6,7两种，但最长公共子序列的长度是4。
+         * 
+         * 解题思路：
+         * 令A=[0,...,x] B=[0,...,y] LCS(x,y)表示以A[x]和B[y]结尾的最长公共子序列
+         * 则 LCS(x,y) = 
+            (1) LCS(x - 1,y - 1) + 1 如果Ax ＝ By
+            (2) max(LCS(x – 1, y) , LCS(x, y – 1)) 如果Ax ≠ By
+            (3) 0 如果x = 0或者y = 0
+         * 
+         * ref:https://blog.csdn.net/lxt_Lucia/article/details/81209962
+         */
+        public static int LongestCommonSubsequnce(int[] A, int[] B)
+        {
+            int ans = 0;
+            //第0行和第0列是全0值，哨兵，方便下标计算不越界！！
+            int[,] LCS = new int[A.Length + 1, B.Length + 1];
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = 0; j < B.Length; j++)
+                {
+                    int x = i + 1;
+                    int y = j + 1;
+                    if (A[i] == B[j])
+                    {
+                        LCS[x, y] = LCS[x - 1, y - 1] + 1;
+                        if (LCS[x, y] > ans) ans = LCS[x, y];
+                    }
+                    else
+                    {
+                        LCS[x, y] = Math.Max(LCS[x - 1, y], LCS[x, y - 1]);
+                    }
+                }
+            }
+            return ans;
         }
 
         /*
@@ -68,7 +166,7 @@ namespace Algorithm
          */
         public static int BoxStacking(int[] H, int[] W, int[] D)
         {
-
+            return 0;
         }
 
 
@@ -77,9 +175,9 @@ namespace Algorithm
          * 有 n 个物品和一个大小为 m 的背包. 给定数组 A 表示每个物品的大小和数组 V 表示每个物品的价值.问最多能装入背包的总价值是多大?
          * 
          */
-        public static int BackPackI(int[] A,int[] V, int M)
+        public static int BackPackI(int[] A, int[] V, int M)
         {
-
+            return 0;
         }
 
         /*
@@ -88,7 +186,7 @@ namespace Algorithm
          */
         public static int BackPackII(int[] A, int[] V, int M)
         {
-
+            return 0;
         }
 
         /*
@@ -99,7 +197,7 @@ namespace Algorithm
 
         public static int BalancedPartition(int[] arr)
         {
-
+            return 0;
         }
 
         /*
@@ -117,7 +215,7 @@ namespace Algorithm
          */
         public static int EditDistance()
         {
-
+            return 0;
         }
 
 
@@ -129,7 +227,7 @@ namespace Algorithm
          */
         public static int BooleanParenthesizations(string exp)
         {
-
+            return 0;
         }
 
         /*
@@ -140,7 +238,7 @@ namespace Algorithm
          */
         public static int OptimalStrategyForGame(int[] arr)
         {
-
+            return 0;
         }
     }
 }
---
+++ TestAlgorithm/TestDynamicProgramming.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm;
+
+namespace TestAlgorithm
+{
+    [TestClass]
+    public class TestDynamicProgramming
+    {
+
+
+        [TestMethod]
+        public void TestMakeChange()
+        {
+            int[] coins = new[] {2,3,5 };
+            int amount = 12;
+            int ans = DynamicPrograming.MakeChange(coins,amount);
+            Assert.IsTrue(ans == 3);
+            amount = 11;
+            ans = DynamicPrograming.MakeChange(coins, amount);
+            Assert.IsTrue(ans == 3);
+            amount = 1;
+            ans = DynamicPrograming.MakeChange(coins, amount);
+            Assert.IsTrue(ans == -1);
+        }
+
+        [TestMethod]
+        public void TestMaxContiguousSubsequence()
+        {
+            int[] arr = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            int ans = DynamicPrograming.MaxContiguousSubsequence(arr);
+            Assert.IsTrue(ans == 6);
+        }
+
+        [TestMethod]
+        public void TestLongestIncreasingSubsequence()
+        {
+            int[] arr = new[] { 10, 9, 2, 5, 3, 7, 101, 18 };
+            int ans = DynamicPrograming.LongestIncreasingSubsequence(arr);
+            Assert.IsTrue(ans == 4);
+
+            arr = new[] { 10, 1, 8, 3, 5, 6, 2, 7, 3 };
+            ans = DynamicPrograming.LongestIncreasingSubsequence(arr);
+            Assert.IsTrue(ans == 5);
+
+            arr = new[] { 1, 1, 1, 1, 1, 1 };
+            ans = DynamicPrograming.LongestIncreasingSubsequence(arr);
+            Assert.IsTrue(ans == 1);
+        }
+
+        [TestMethod]
+        public void TestLongestCommonSubsequence()
+        {
+            int[] a = new[] { 1, 3, 5, 4, 2, 6, 8, 7 };
+            int[] b = new[] { 1, 4, 8, 6, 7, 5 };
+            int ans = DynamicPrograming.LongestCommonSubsequnce(a, b);
+            Assert.IsTrue(ans == 4);
+
+        }
+    }
+}
