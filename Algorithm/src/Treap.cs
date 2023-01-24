216d99200b7ac80f12f4ba040ddbb2c6329f6915
jwk000 <j__w__k@163.com>
2023-01-24 14:49:18 +0800

    修改树堆bug

--- Algorithm/src/Treap.cs
+++ Algorithm/src/Treap.cs
@@ -26,53 +26,57 @@ namespace Algorithm
 
             public override string ToString()
             {
-                return Key.ToString();
+                return $"{Key}|{Priority}|{Parent?.Key}";
             }
         }
 
         Random mRander = new Random();
         TPNode mRoot;
 
-        public void Add(int key, int val)
+        public void Add(int key, int val, int pr=0)
         {
             var node = new TPNode(key, val);
-            node.Priority = mRander.Next(0,1000);
+            node.Priority =pr>0?pr: mRander.Next(0,1000);
+            Console.WriteLine("Add {0}", node);
             if (mRoot == null)
             {
                 mRoot = node;
-                return;
             }
-            var p = mRoot;
-            while (p != null)
+            else
             {
-                if (p.Key == key)
+                var p = mRoot;
+                while (p != null)
                 {
-                    return;
-                }
-                if (p.Key < key)
-                {
-                    if (p.Right == null)
+                    if (p.Key == key)
                     {
-                        p.Right = node;
-                        node.Parent = p;
-                        break;
+                        return;
                     }
-                    p = p.Right;
-                }
-                else
-                {
-                    if (p.Left == null)
+                    if (p.Key < key)
                     {
-                        p.Left = node;
-                        node.Parent = p;
-                        break;
+                        if (p.Right == null)
+                        {
+                            p.Right = node;
+                            node.Parent = p;
+                            break;
+                        }
+                        p = p.Right;
                     }
-                    p = p.Left;
+                    else
+                    {
+                        if (p.Left == null)
+                        {
+                            p.Left = node;
+                            node.Parent = p;
+                            break;
+                        }
+                        p = p.Left;
+                    }
                 }
+
+                FixHeap(node);
+
             }
-
-            FixHeap(node);
-
+            Console.WriteLine(Print());
         }
 
         void FixHeap(TPNode node)
@@ -85,11 +89,11 @@ namespace Algorithm
                 }
                 if (node == node.Parent.Left)
                 {
-                    RightRotate(node);
+                    RightRotate(node.Parent);
                 }
                 else
                 {
-                    LeftRotate(node);
+                    LeftRotate(node.Parent);
                 }
             }
             mRoot = node;
@@ -98,6 +102,7 @@ namespace Algorithm
         //删除的永远是叶子节点，不影响heap性质
         public void Remove(int key)
         {
+            Console.Write("remove key {0} ", key);
             var p = mRoot;
             while (p != null)
             {
@@ -125,25 +130,29 @@ namespace Algorithm
                     p = p.Left;
                 }
             }
-
         }
-        void RemoveNode(TPNode p)
+        void RemoveNode(TPNode node)
         {
-            if (p.Parent != null)
+            Console.WriteLine("Remove Node {0}", node);
+
+            if (node.Parent != null)
             {
-                if (p == p.Parent.Left)
+                if (node == node.Parent.Left)
                 {
-                    p.Parent.Left = null;
+                    node.Parent.Left = node.Left;
+                    if(node.Left!=null) node.Left.Parent = node.Parent;
                 }
                 else
                 {
-                    p.Parent.Right = null;
+                    node.Parent.Right = node.Right;
+                    if(node.Right!=null) node.Right.Parent = node.Parent;
                 }
             }
             else
             {
                 mRoot = null;
             }
+            Console.WriteLine(Print());
 
         }
 
@@ -178,36 +187,33 @@ namespace Algorithm
         void LeftRotate(TPNode node)
         {
             var P = node.Parent;
-            var L = node.Left;
-            var PP = P.Parent;
-            if (PP != null)
+            var R = node.Right;
+            if (P != null)
             {
-                if (PP.Left == P) PP.Left = node;
-                else PP.Right = node;
+                if (P.Left == node) P.Left = R;
+                else P.Right = R;
             }
-            node.Parent = P.Parent;
-            node.Left = P;
-            P.Right = L;
-            if (L != null) L.Parent = P;
-            P.Parent = node;
+            node.Parent = R;
+            node.Right = R.Left;
+            if (R.Left != null) R.Left.Parent = node;
+            R.Parent = P;
+            R.Left = node;
         }
 
         void RightRotate(TPNode node)
         {
             var P = node.Parent;
-            var R = node.Right;
-            var PP = P.Parent;
-            if (PP != null)
+            var L = node.Left;
+            if (P != null)
             {
-                if (PP.Left == P) PP.Left = node;
-                else PP.Right = node;
+                if (P.Left == node) P.Left = L;
+                else P.Right = L;
             }
-            node.Parent = P.Parent;
-            node.Right = P;
-            P.Left = R;
-            if (R != null) R.Parent = P;
-            P.Parent = node;
-
+            node.Parent = L;
+            node.Left = L.Right;
+            if (L.Right != null) L.Right.Parent = node;
+            L.Parent = P;
+            L.Right = node;
         }
 
         public int Search(int key)
@@ -232,6 +238,88 @@ namespace Algorithm
             return -1;
         }
 
+        public int Height()
+        {
+            return _height(mRoot);
+        }
+
+        int _height(TPNode node)
+        {
+            if (node == null) return 0;
+            int hl = _height(node.Left) + 1;
+            int hr = _height(node.Right) + 1;
+            return Math.Max(hl, hr);
+        }
+        public string Print()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (mRoot != null)
+            {
+                int h = Height();
+                List<TPNode> nodes = new List<TPNode>();
+                nodes.Add(mRoot);
+                for (int i = 0; i < (1 << h - 1) - 1; i++)
+                {
+                    var node = nodes[i];
+                    if (node != null)
+                    {
+                        nodes.Add(node.Left);
+                        nodes.Add(node.Right);
+                    }
+                    else
+                    {
+                        nodes.Add(null);
+                        nodes.Add(null);
+                    }
+                }
+
+                int st = 0;//0输出数字 1输出连线
+                for (int i = 0; i < h; i++)//行数
+                {
+                    int j = (1 << (h - i - 1)) - 1;//每行开头空格数
+                    for (int k = 0; k < (1 << i); k++)//每行节点数
+                    {
+                        int m = k + (1 << i) - 1;//下标=k+起始下标
+                        int z = k == 0 ? j : j * 2 + 1;//间隔空格数
+                        for (int n = 0; n < z; n++)
+                        {
+                            sb.Append("  ");
+                        }
+                        if (nodes[m] != null)
+                        {
+                            if (st == 0)
+                            {
+
+                                sb.Append(nodes[m]);
+                            }
+                            else
+                            {
+                                sb.Append(m % 2 > 0 ? '/' : '\\').Append("  ");
+                            }
+                        }
+                        else
+                        {
+                            sb.Append("  ");
+                        }
+                    }
+                    sb.AppendLine();
+
+                    if (st == 0)
+                    {
+                        st = 1;
+                    }
+                    else
+                    {
+                        st = 0;
+                        i--;
+                    }
+                }
+            }
+
+
+            return sb.ToString();
+        }
 
 
     }
--- TestAlgorithm/TestTreap.cs
+++ TestAlgorithm/TestTreap.cs
@@ -11,26 +11,53 @@ namespace TestAlgorithm
     public class TestTreap
     {
         [TestMethod]
-        public void Test()
+        public void TestTreap1()
         {
             Treap treap = new Treap();
+            int[] pr = new[] { 72, 118, 934, 663, 374, 323, 941, 861, 7 };
+            for(int i = 1; i < 10; i++)
+            {
+                treap.Add(i, i * 100,pr[i-1]);
+            }
 
-            for(int i = 1; i < 20; i++)
+            int v = treap.Search(8);
+            Assert.IsTrue(v == 800);
+
+            for(int i = 0; i < 9; i++)
+            {
+                treap.Remove(i);
+                int x = treap.Search(i);
+                Assert.IsTrue(x == -1);
+            }
+
+            int w = treap.Search(9);
+            Assert.IsTrue(w == 900);
+
+        }
+
+        [TestMethod]
+        public void TestTreap2()
+        {
+            Treap treap = new Treap();
+            for (int i = 1; i < 10; i++)
             {
                 treap.Add(i, i * 100);
             }
 
-            int v = treap.Search(10);
-            Assert.IsTrue(v == 1000);
+            int v = treap.Search(8);
+            Assert.IsTrue(v == 800);
 
-            for(int i = 0; i < 10; i++)
+            for (int i = 0; i < 9; i++)
             {
                 treap.Remove(i);
+                int x = treap.Search(i);
+                Assert.IsTrue(x == -1);
             }
 
-            int w = treap.Search(10);
-            Assert.IsTrue(w == 1000);
+            int w = treap.Search(9);
+            Assert.IsTrue(w == 900);
 
         }
+
     }
 }
