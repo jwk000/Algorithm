612dbe456ff6e057a26ef33302ccf74087599ae8
jwk000 <j__w__k@163.com>
2023-01-25 20:16:43 +0800

    Floyd算法，多源最短路径

---
+++ Algorithm/src/Floyd.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    //Floyd 算法是一个基于「贪心」、「动态规划」求一个图中 所有点到所有点 最短路径的算法，时间复杂度 O(n3)
+    public class Floyd
+    {
+        int[,] G; //下标是顶点编号，值是距离；-1表示不可达，0是i->i的距离，>0是i->j的距离
+        int[,] dist;//记录i->j最短距离
+        int[,] path;//记录i->j最短路径经过的中转点k，递归查找i->k,k->j的中转点就得到了path
+        int N;
+        public Floyd(int[,] map, int n)
+        {
+            N = n;
+            G = new int[n, n];
+            dist = new int[n, n];
+            path = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    G[i, j] = map[i, j];
+                    dist[i, j] = G[i, j];
+                    if (G[i, j] > 0)
+                    {
+                        path[i, j] = j;
+                    }
+                    else
+                    {
+                        path[i, j] = -1;
+                    }
+                }
+            }
+
+            //遍历所有通过k中转的路径，检查是否有更短路径
+            for (int k = 0; k < n; k++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (dist[i, k] > 0 && dist[k, j] > 0 && (dist[i, j] < 0 || dist[i, k] + dist[k, j] < dist[i, j]))
+                        {
+                            dist[i, j] = dist[i, k] + dist[k, j];
+                            path[i, j] = k;
+                        }
+                    }
+                }
+            }
+        }
+
+        //i->j最短路径
+        public List<int> GetPath(int i, int j)
+        {
+            List<int> ret = new List<int>();
+            if (i < 0 || i >= N || j < 0 || j >= N)
+            {
+                return ret;
+            }
+            if (path[i, j] < 0)
+            {
+                return ret;
+            }
+            ret.Add(i);
+            search(i, j, ret);
+            return ret;
+        }
+
+        void search(int i, int j, List<int> ret)
+        {
+            int k = path[i, j];
+            if (k != j)
+            {
+                search(i, k, ret);
+                search(k, j, ret);
+            }
+            else
+            {
+                ret.Add(j);
+            }
+        }
+    }
+}
---
+++ TestAlgorithm/TestFloyd.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm;
+
+namespace TestAlgorithm
+{
+    [TestClass]
+    public class TestFloyd
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            //0-1只有唯一路径可达
+            int[,] map = new int[5, 5]
+            {
+                { 0,-1,1,-1,-1 },
+                {-1,0,-1,1,-1 },
+                { 1,-1,0,-1,1 },
+                { -1,1,-1,0,1 },
+                {-1,-1,1,1,0 } };
+
+            Floyd floyd = new Floyd(map,5);
+
+            var path = floyd.GetPath(0, 1);
+            Assert.IsTrue(string.Join(',', path) == "0,2,4,3,1");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            //0-1有多条路径可达
+            int[,] map = new int[5, 5]
+            {
+                { 0,-1,1,3,-1 },
+                {-1,0,8,-1,2 },
+                { 1,8,0,1,-1 },
+                { 3,-1,1,0,2 },
+                {-1,2,-1,2,0 } };
+
+            Floyd floyd = new Floyd(map, 5);
+
+            var path = floyd.GetPath(0, 1);
+            Assert.IsTrue(string.Join(',', path) == "0,2,3,4,1");
+
+        }
+    }
+}
